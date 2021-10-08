d8fce1dd6a6127e3222d8d142811a2d413329db7
tlgbltcn <tbolatcan@gmail.com>
2021-10-08 22:43:26 +0300

    hilt has been initialized.

---
+++ app/src/main/java/com/tlgbltcn/githubsquarerepos/di/NetworkModule.kt
@@ -0,0 +1,11 @@
+package com.tlgbltcn.githubsquarerepos.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    // It will fill out with providers.
+}
